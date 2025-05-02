0a7ba76141871f3888c84c31eaef446bd39784f8
lapfelix <felix@lapal.me>
2025-05-02 12:14:21 -0400

    Improve trimming algo

--- trim_xcode_build.sh
+++ trim_xcode_build.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 cwd=$(pwd)
 prefix="${cwd%/}/"
-# default ignore regex and fallback pattern for trimming paths
-ignore_regex='Firebase.*AST'
-fallback_pattern='/Classes/'
+# default ignore regex and fallback pattern (none)
+ignore_regex=''
+fallback_pattern=''
 
 # parse options: -i | --ignore, -f | --fallback, and optional infile
 infile='-'
@@ -30,24 +30,60 @@ while (( $# > 0 )); do
   esac
 done
 
-gawk -v prefix="$prefix" \
+awk -v prefix="$prefix" \
      -v ignore_regex="$ignore_regex" \
      -v fallback_pattern="$fallback_pattern" \
-' 
-BEGIN { errors = 0 }
+'
+BEGIN { errors = 0; ctx_len = 0 }
+# buffer include-chain context lines
+/^In file included from / {
+  # trim prefix or fallback from context path
+  ctx_line = $0
+  if (substr(ctx_line, 1, length(prefix)) == prefix) {
+    ctx_line = substr(ctx_line, length(prefix) + 1)
+  } else if (fallback_pattern != "" && (pos = index(ctx_line, fallback_pattern))) {
+    ctx_line = substr(ctx_line, pos + length(fallback_pattern))
+  }
+  ctx_len++
+  ctx[ctx_len] = ctx_line
+  next
+}
 /error:/ {
-  if ($0 ~ ignore_regex) next
-  if (match($0, /^(.+):([0-9]+):([0-9]+):[[:space:]]*error:[[:space:]]*(.*)$/, arr)) {
-    path = arr[1]; ln = arr[2]; col = arr[3]; msg = arr[4]
-    if (substr(path, 1, length(prefix)) == prefix) {
-      path = substr(path, length(prefix) + 1)
-    } else {
-      pos = index(path, fallback_pattern)
-      if (pos) path = substr(path, pos + 1)
-    }
-    print path ":" ln ":" col ": error: " msg
+  if (ignore_regex != "" && $0 ~ ignore_regex) next
+  # print buffered include-context, then clear
+  for (i = 1; i <= ctx_len; i++) print ctx[i]
+  ctx_len = 0
+  # parse and trim error lines path:line:col: error: message
+  line = $0
+  p1 = index(line, ":")
+  if (p1 == 0) {
+    print line
   } else {
-    print $0
+    path = substr(line, 1, p1-1)
+    rest1 = substr(line, p1+1)
+    p2 = index(rest1, ":")
+    if (p2 == 0) {
+      print line
+    } else {
+      ln = substr(rest1, 1, p2-1)
+      rest2 = substr(rest1, p2+1)
+      p3 = index(rest2, ":")
+      if (p3 == 0) {
+        print line
+      } else {
+        col = substr(rest2, 1, p3-1)
+        rest3 = substr(rest2, p3+1)
+        sub(/^[[:space:]]*/, "", rest3)
+        sub(/^error:[[:space:]]*/, "", rest3)
+        msg = rest3
+        if (substr(path, 1, length(prefix)) == prefix) {
+          path = substr(path, length(prefix) + 1)
+        } else if (fallback_pattern != "" && (pos = index(path, fallback_pattern))) {
+          path = substr(path, pos + length(fallback_pattern))
+        }
+        print path ":" ln ":" col ": error: " msg
+      }
+    }
   }
   while (getline nl > 0) {
     if (nl ~ /^[[:space:]]*$/) continue
@@ -63,7 +99,10 @@ BEGIN { errors = 0 }
   errors = 1
 }
 END {
-  if (errors) exit 1
+  if (errors) {
+    print "BUILD FAILED"
+    exit 1
+  }
   print "BUILD SUCCEEDED"
   exit 0
 }
